d9b9af568a9195f04d59a52bd120c490b722c134
Flavio Grilli <flavio.grilli@ynov.com>
2025-09-15 11:24:18 +0200

    y a des trucs

--- src/character/enemies.go
+++ src/character/enemies.go
@@ -1,18 +1,5 @@
 package character
 
-type Character struct {
-	Name      string
-	Class     string
-	Level     int
-	Hp        int
-	HpMax     int
-	Inventory []Item
-}
-
-type Item struct {
-	Name     string
-	Quantity int
-}
 type Enemy struct {
 	Name   string
 	MaxHp  int
@@ -21,5 +8,5 @@ type Enemy struct {
 }
 
 func (enemy1 *Enemy) initEnemy() {
-	*enemy1 = Enemy{"Recrue", 100, 80, 5}
+	*enemy1 = Enemy{"Recrue", 50, 50, 5}
 }
