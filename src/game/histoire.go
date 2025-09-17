c82af9c6df20be66d662b796e55b23ee1460a20c
Lisa Payan <lisa.payan@ynov.com>
2025-09-17 15:45:45 +0200

    Histoire gardiens Artémis et Héphaïstos

--- src/game/histoire.go
+++ src/game/histoire.go
@@ -3,5 +3,17 @@ package game
 import "fmt"
 
 func Intro() {
-	fmt.Println("Les dieux, autrefois protecteurs de l'humanité, ont été corrompus par une force maléfique. Notre monde est désormais plongé dans le chaos et seul un héros choisi par le destin peut le sauver. Pour cela il lui faudra les affronter et les tuer.  C'est le seul moyen de ramener la paix.\n")
+	fmt.Print("Les dieux, autrefois protecteurs de l'humanité, ont été corrompus par une force maléfique. Notre monde est désormais plongé dans le chaos et seul un héros choisi par le destin peut le sauver. Pour cela il lui faudra les affronter et les tuer.  C'est le seul moyen de ramener la paix.\n\n")
+}
+
+func GardinensArtemis() {
+	fmt.Print("Tu pénètres dans une vaste forêt ancienne, baignée d'une lumière lunaire surnaturelle. Ce territoire sacré est le domaine d'Artémis, déesse de la chasse. Avant d'atteindre la déesse, le joueur doit affronter deux gardiens")
+}
+
+func PremierGardien() {
+
+}
+
+func GardiensHéphaïstos() {
+	fmt.Print("Tu pénètres un vaste atelier sacré où résonnent les coups de marteau divins. Ce lieu, dédié à Héphaïstos, dieu du feu et de la forge. Avant d'atteindre la déesse, le joueur doit affronter deux gardiens.")
 }
