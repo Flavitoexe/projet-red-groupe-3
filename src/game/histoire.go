18e722d72cebabc53ac4d388c7e6b2af8aa33729
Lisa Payan <lisa.payan@ynov.com>
2025-09-17 16:51:51 +0200

    Histoire des dieux.

--- src/game/histoire.go
+++ src/game/histoire.go
@@ -10,16 +10,32 @@ func GardinensArtemis() {
 	fmt.Print("Tu pénètres dans une vaste forêt ancienne, baignée d'une lumière lunaire surnaturelle. Ce territoire sacré est le domaine d'Artémis, déesse de la chasse. Avant d'atteindre la déesse, tu dois affronter deux gardiens")
 }
 
+func DieuArtémis() {
+	fmt.Print("Devant toi se tient Artémis, déesse de la chasse, autrefois protectrice. Son regard glacé promet un combat sans merci. Gagneras-tu ?")
+}
+
 func GardiensHéphaïstos() {
 	fmt.Print("Tu pénètres un vaste atelier sacré où résonnent les coups de marteau divins. Ce lieu est dédié à Héphaïstos, dieu du feu et de la forge. Avant d'atteindre la déesse, tu dois affronter deux gardiens.")
+}
+
+func DieuHéphaïstos() {
+	fmt.Print("Devant toi, Héphaïstos, dieu de la forge, autrefois artisan des merveilles divines. Ses outils brûlants annoncent un combat brutal et sans pitié. Gagneras-tu ?")
 }
 
 func GardiensArès() {
 	fmt.Print("Tu pénètres dans un fort militaire, vaste et imposant, construit sur un plateau battu par le vent. Ce lieu est dédié à Arès, dieu de la guerre. Pour atteindre Arès, tu dois traverser le camp contrôlé par ses gardiens et les affronter.")
 }
 
+func DieuArès() {
+	fmt.Print("Devant toi se dresse Arès, dieu de la guerre, incarnation de la rage et du combat. Son regard brûle d’une soif de bataille sans fin. Prépare-toi à un affrontement féroce. Bonne chance à toi.")
+}
+
 func GardiensHadès() {
-	fmt.Print("Le joueur s’enfonce dans les profondeurs sombres du Royaume des morts, un monde ténébreux fait de pierre noire, de flammes froides et de brumes éternelles. Ce territoire funeste est gouverné par Hadès, dieu des enfers. Avant d'atteindre Hadès, tu dois affronter ses gardiens fous.")
+	fmt.Print("Le joueur s'enfonce dans les profondeurs sombres du Royaume des morts, un monde ténébreux fait de pierre noire, de flammes froides et de brumes éternelles. Ce territoire funeste est gouverné par Hadès, dieu des enfers. Avant d'atteindre Hadès, tu dois affronter ses gardiens fous.")
+}
+
+func DieuHadès() {
+	fmt.Print("Devant toi se tient Hadès, souverain des enfers, maître des âmes perdues. Son regard froid et impénétrable plonge dans les ténèbres de ton âme. Le jugement final approche.")
 }
 
 func PremierGardien() {
