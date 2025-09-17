3bcc48bae479134863575d51f0877e6055a9f26c
Lisa Payan <lisa.payan@ynov.com>
2025-09-17 16:26:32 +0200

    Histoire gardiens Arès et Hadès

--- src/game/histoire.go
+++ src/game/histoire.go
@@ -7,13 +7,21 @@ func Intro() {
 }
 
 func GardinensArtemis() {
-	fmt.Print("Tu pénètres dans une vaste forêt ancienne, baignée d'une lumière lunaire surnaturelle. Ce territoire sacré est le domaine d'Artémis, déesse de la chasse. Avant d'atteindre la déesse, le joueur doit affronter deux gardiens")
+	fmt.Print("Tu pénètres dans une vaste forêt ancienne, baignée d'une lumière lunaire surnaturelle. Ce territoire sacré est le domaine d'Artémis, déesse de la chasse. Avant d'atteindre la déesse, tu dois affronter deux gardiens")
+}
+
+func GardiensHéphaïstos() {
+	fmt.Print("Tu pénètres un vaste atelier sacré où résonnent les coups de marteau divins. Ce lieu est dédié à Héphaïstos, dieu du feu et de la forge. Avant d'atteindre la déesse, tu dois affronter deux gardiens.")
+}
+
+func GardiensArès() {
+	fmt.Print("Tu pénètres dans un fort militaire, vaste et imposant, construit sur un plateau battu par le vent. Ce lieu est dédié à Arès, dieu de la guerre. Pour atteindre Arès, tu dois traverser le camp contrôlé par ses gardiens et les affronter.")
+}
+
+func GardiensHadès() {
+	fmt.Print("Le joueur s’enfonce dans les profondeurs sombres du Royaume des morts, un monde ténébreux fait de pierre noire, de flammes froides et de brumes éternelles. Ce territoire funeste est gouverné par Hadès, dieu des enfers. Avant d'atteindre Hadès, tu dois affronter ses gardiens fous.")
 }
 
 func PremierGardien() {
 
-}
-
-func GardiensHéphaïstos() {
-	fmt.Print("Tu pénètres un vaste atelier sacré où résonnent les coups de marteau divins. Ce lieu, dédié à Héphaïstos, dieu du feu et de la forge. Avant d'atteindre la déesse, le joueur doit affronter deux gardiens.")
 }
