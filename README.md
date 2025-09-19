8a2cc3dcbab0f521b8b13b69b948214d4b2ccf0b
Eddinee <edssee008@gmail.com>
2025-09-19 12:01:39 +0200

    RREEAADDMMEE

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
-# projet-red-groupe-3
+# ANIMUS
+ Présentation : Corrompus, les dieux ont plongé le monde dans le chaos. Un héros doit les vaincre pour restaurer la paix
 
+Pour cela il devra récolter des objets puissants pour battre plus facilement les dieux
+
+
+--- 
+# Les fonctionnalités
+Le jeu possèdera des fonctions tels que le magasis, le forgeron pour crafter des items ou bien son inventaire (il stockera ses items ).
+
+
+
+## Prérequis. 
+test ***test*** test
 test
--- src/main.go
+++ src/main.go
@@ -3,18 +3,5 @@ package main
 import "projet-red/game"
 
 func main() {
-	p1 := game.Character{}
-	// p1.InitCharacter()
-	game.Intro()
-	p1.CharacterCreation()
-	//  p1.MainMenu()
-	// e1 := game.Enemy{}
-	// e1.InitEnemy()
-	// game.TrainingFight(p1, e1)
-	// game.AccessShop()
-	// p1.AccesInventory()
-	p1.AddInventory(game.Item{"Epée plus", 1, "Arme"})
-	// p1.AccesInventory()
-	p1.RemoveInventory(game.Item{"Flèches classiques", 10, "Cons"}, 4)
-	p1.AccesInventory()
+	game.AccessShop()
 }
